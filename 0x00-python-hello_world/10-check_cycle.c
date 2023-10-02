724950056dda20f0b051abf1b851f68a0e05324b
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-10-02 12:08:27 -0700

    small changes

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - function checks if a singly linked list has a cycle int it.
- * @list: pointer to the beginning of the node
- * Return: 0 if no cycle, 1 if there is a cycle
+ * check_cycle - checks to see if a list is in an endless loop or cycle
+ * @list: the list to check
+ * Return: 0 if no cycle is detected, 1 if there is a cycle
  */
+
 int check_cycle(listint_t *list)
 {
-listint_t *slow = list;
-listint_t *fast = list;
-if (!list)
-	return (0);
-while (slow && fast &&fast->next)
+listint_t *doub = list;
+listint_t *reg = list;
+
+if (list == NULL)
+return (0);
+
+while (doub && doub->next)
 {
-	slow = slow->next;
-	fast = fast->next->next;
-if (slow == fast)
-	return (1);
+reg = reg->next;
+doub = doub->next->next;
+
+if (reg == doub)
+return (1);
 }
 return (0);
 }
